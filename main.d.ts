1ecfc4379e36440a2216e27d00fba2199120f696
Robin <hello@robinblomberg.se>
2020-12-07 22:22:56 +0100

    Add type assertion to equal()

--- main.d.ts
+++ main.d.ts
@@ -5,7 +5,7 @@
 
 export const doesNotThrow: (fn: Function) => void;
 
-export const equal: (a: any, b: any) => void;
+export const equal: <T>(a: any, b: T) => asserts a is T;
 
 export const notEqual: (a: any, b: any) => void;
 
